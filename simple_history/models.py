e734c0551383af9f27f3ed7a94f90727ac56b30e
bskripal <skipalboris@gmail.com>
2016-09-07 12:14:16 +0300

    Изменения из https://github.com/Kyruus/django-simple-history/commit/5ba8d2b4d72819f154a11f297796e6a2bb7172bf

--- simple_history/models.py
+++ simple_history/models.py
@@ -13,6 +13,7 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.utils.encoding import smart_text
 from django.utils.timezone import now
 from django.utils.translation import string_concat
+from simple_history import register
 
 try:
     from django.apps import apps
@@ -36,11 +37,12 @@ class HistoricalRecords(object):
     thread = threading.local()
 
     def __init__(self, verbose_name=None, bases=(models.Model,),
-                 user_related_name='+', table_name=None, inherit=False):
+                 user_related_name='+', table_name=None, inherit=False, m2m_fields=None):
         self.user_set_verbose_name = verbose_name
         self.user_related_name = user_related_name
         self.table_name = table_name
         self.inherit = inherit
+        self.m2m_fields = m2m_fields
         try:
             if isinstance(bases, six.string_types):
                 raise TypeError
@@ -54,6 +56,7 @@ class HistoricalRecords(object):
         self.cls = cls
         models.signals.class_prepared.connect(self.finalize, weak=False)
         self.add_extra_methods(cls)
+        self.setup_m2m_history(cls)
 
     def add_extra_methods(self, cls):
         def save_without_historical_record(self, *args, **kwargs):
@@ -70,6 +73,18 @@ class HistoricalRecords(object):
             return ret
         setattr(cls, 'save_without_historical_record',
                 save_without_historical_record)
+
+    def setup_m2m_history(self, cls):
+        m2m_history_fields = self.m2m_fields
+        if m2m_history_fields:
+            assert (isinstance(m2m_history_fields, list) or isinstance(m2m_history_fields,
+                                                                       tuple)), 'm2m_history_fields must be a list or tuple'
+        for field_name in m2m_history_fields:
+            field = getattr(cls, field_name).field
+            assert isinstance(field, models.fields.related.ManyToManyField), ('%s must be a ManyToManyField' % field_name)
+            if not sum([isinstance(item, HistoricalRecords) for item in field.rel.through.__dict__.values()]):
+                field.rel.through.history = HistoricalRecords()
+                register(field.rel.through)
 
     def finalize(self, sender, **kwargs):
         try:
@@ -94,6 +109,8 @@ class HistoricalRecords(object):
         models.signals.post_save.connect(self.post_save, sender=sender,
                                          weak=False)
         models.signals.post_delete.connect(self.post_delete, sender=sender,
+                                           weak=False)
+        models.signals.m2m_changed.connect(self.m2m_changed, sender=sender,
                                            weak=False)
 
         descriptor = HistoryDescriptor(history_model)
@@ -241,6 +258,27 @@ class HistoricalRecords(object):
 
     def post_delete(self, instance, **kwargs):
         self.create_historical_record(instance, '-')
+
+    def m2m_changed(self, action, instance, sender, **kwargs):
+        source_field_name, target_field_name = None, None
+        for field_name, field_value in sender.__dict__.items():
+            if isinstance(field_value, models.fields.related.ReverseSingleRelatedObjectDescriptor):
+                if field_value.field.related.parent_model == kwargs['model']:
+                    target_field_name = field_name
+                elif field_value.field.related.parent_model == type(instance):
+                    source_field_name = field_name
+        items = sender.objects.filter(**{source_field_name: instance})
+        if kwargs['pk_set']:
+            items = items.filter(**{target_field_name + '__id__in': kwargs['pk_set']})
+        for item in items:
+            if action == 'post_add':
+                if hasattr(item, 'skip_history_when_saving'):
+                    return
+                self.create_historical_record(item, '+')
+            elif action == 'pre_remove':
+                self.create_historical_record(item, '-')
+            elif action == 'pre_clear':
+                self.create_historical_record(item, '-')
 
     def create_historical_record(self, instance, history_type):
         history_date = getattr(instance, '_history_date', now())
